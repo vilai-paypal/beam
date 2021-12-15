39d4293b1aacb961732444dcd19b73c6674f1efa
Victor <vilai@paypal.com>
2021-12-15 13:20:03 -0800

    Support Spanner JSON

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerSchema.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/SpannerSchema.java
@@ -175,6 +175,9 @@ abstract class SpannerSchema implements Serializable {
       if ("NUMERIC".equals(spannerType)) {
         return Type.numeric();
       }
+      if ("JSON".equals(spannerType)) {
+        return Type.json(); // https://github.com/googleapis/java-spanner/blob/main/google-cloud-spanner/src/main/java/com/google/cloud/spanner/Type.java#L100
+      }
 
       if (spannerType.startsWith("ARRAY")) {
         // Substring "ARRAY<xxx>"
